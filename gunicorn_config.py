71f7bd17c93a3144650bf68778f93587828868fe
tyleringram123 <64450754+tyleringram123@users.noreply.github.com>
2025-12-22 12:06:08 -0600

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@
 import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', '8080')}"
-workers = int(os.getenv("WEB_CONCURRENCY", "1"))  # keep 1 on 512MB
-timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
+workers = 1
+timeout = 120
